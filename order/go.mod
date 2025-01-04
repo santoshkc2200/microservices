47aaf3a84fdf46e88ba11e77962d25e947970463
Santosh KC <santoshkc2200@gmail.com>
2025-01-04 18:19:49 +0900

    updated module

--- order/go.mod
+++ order/go.mod
@@ -1,3 +1,3 @@
-module GitHub.com/santoshkc2200/microservices/order
+module gitHub.com/santoshkc2200/microservices/order
 
 go 1.23.4
---
+++ order/internal/ports/db.go
@@ -0,0 +1,12 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/santoshkc2200/microservices/order/internal/application/core/domain"
+)
+
+type DBPort interface {
+	Get(ctx context.Context, id int64) (domain.Order, error)
+	Save(context.Context, *domain.Order) error
+}
